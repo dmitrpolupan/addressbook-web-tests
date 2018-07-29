98ea77f45f624ad7a7768afe19038ac4ed63e9e9
dmitrpolupan <dmitr.polupan@gmail.com>
2018-07-29 21:18:02 +0300

    group creation

---
+++ addressbook-web-tests/AccountName.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_tests
+{
+    class AccountName
+    {
+        private string name;
+        private string password;
+        
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+            }
+        }
+
+        public AccountName(string name, string password)
+        {
+            this.name = name;
+            this.password = password;
+        }
+
+    }
+}
--- addressbook-web-tests/UnitTest2.cs
+++ addressbook-web-tests/UnitTest2.cs
@@ -30,14 +30,57 @@ namespace addressbook_web_tests
         [Test]
         public void FirstTest()
         {
-            
-            driver.Url = "http://localhost/addressbook/";
+            NavigateToPage("http://localhost/addressbook/");
+            Login(new AccountName("admin", "secret"));
+            GoToGroupPage();
+            InitGroupCreation();
+            FillInNewGroup(new GroupData("with all", "header", "footer"));
+            SubmitGroupCreation();
+            ReturnToGroupPage();
+        }
+
+        private void ReturnToGroupPage()
+        {
+            driver.FindElement(By.LinkText("group page")).Click();
+        }
+
+        private void SubmitGroupCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void FillInNewGroup(GroupData group)
+        {
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
+        }
+
+        private void InitGroupCreation()
+        {
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void GoToGroupPage()
+        {
+            driver.FindElement(By.LinkText("GROUPS")).Click();
+        }
+
+        private void Login(AccountName account)
+        {
             driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
+            driver.FindElement(By.Name("user")).SendKeys(account.Name);
             driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
             driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
-            //wait.Until(ExpectedConditions.TitleIs("My Store"));
+        }
+
+        private void NavigateToPage(string uri)
+        {
+            driver.Url = uri;
         }
 
         [TearDown]
