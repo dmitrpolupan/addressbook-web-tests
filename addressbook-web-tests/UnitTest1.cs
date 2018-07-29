579c0668b85c3e797a5709201723e03db33da73e
dmitrpolupan <dmitr.polupan@gmail.com>
2018-07-29 20:48:21 +0300

    login

--- addressbook-web-tests/UnitTest1.cs
+++ addressbook-web-tests/UnitTest1.cs
@@ -1,16 +1,1 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace addressbook_web_tests
-{
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-           
-            
-        }
-    }
-}
+﻿
---
+++ addressbook-web-tests/UnitTest2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace addressbook_web_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+
+        [SetUp]
+        public void start()
+        {
+            ChromeOptions options = new ChromeOptions();
+            //options.AddArguments("start-fullscreen");
+            driver = new ChromeDriver(options);
+            Console.WriteLine(((IHasCapabilities)driver).Capabilities);
+            //driver = new InternetExplorerDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Manage().Cookies.DeleteAllCookies();
+
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            
+            driver.Url = "http://localhost/addressbook/";
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys("admin");
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys("secret");
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+            //wait.Until(ExpectedConditions.TitleIs("My Store"));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
